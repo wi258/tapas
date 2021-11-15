f6561b88731c15e005f10d1fadabe299f31cce33
Lim <wi258@naver.com>
2021-11-15 17:37:33 +0900

    change code

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var passport = require('./config/passport')
 var util = require('./util')
 var app = express()
 
-const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
